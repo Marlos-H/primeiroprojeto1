e1df166e32c45d9d24c0dbf290176fbe177b96f3
Marlos-H <marlosharingl@gmail.com>
2023-05-16 20:35:12 -0300

    segunda alteração

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("alteraçao");
+        System.out.println("segunda alteraçao2");
     }
 }
