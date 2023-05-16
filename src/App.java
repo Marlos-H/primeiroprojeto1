934141d33ba97502f2513725b359a5f062a38c02
Marlos-H <marlosharingl@gmail.com>
2023-05-16 20:57:43 -0300

    5

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("segunda alteraçao2");
+        System.out.println("segunda alteraçao5");
     }
 }
